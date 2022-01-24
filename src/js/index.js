068cb1fd5d57fe9ae69ff72c56fc89ea7a3f3df2
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-01-25 00:27:30 +0200

    added pagination

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,44 @@
 import '../css/styles.css';
 import Notiflix from 'notiflix';
 import imageTpl from '../templates/imageTpl.hbs';
-import async from 'hbs/lib/async';
+import simpleLightbox from 'simplelightbox';
+import '../../node_modules/simplelightbox/dist/simple-lightbox.min.css';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '25393494-37d8dd7e72c61fe26f3c6ef73';
-
 const gallery = document.querySelector('.gallery');
 const searchBox = document.querySelector('#search-form');
 let imgToFind = '';
+let openModal = null;
 
+gallery.addEventListener('click', openModal);
 searchBox.addEventListener('submit', e => {
   e.preventDefault();
+
   imgToFind = searchBox.elements.searchQuery.value;
   fetchImages(imgToFind)
-    .then(response => {
-      return response.json();
-    })
     .then(imgArr => {
-      createImages(imgArr.hits);
-    });
+      if (imgArr.totalHits === 0) {
+        throw new Error();
+      }
+      notify(imgArr.totalHits);
+      createGallery(imgArr.hits);
+    })
+    .catch(error);
 });
+
+async function fetchImages(keyWord) {
+  const searchFilter = 'image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page';
+  let page = 1;
+  const response = await fetch(`${BASE_URL}?key=${KEY}&q=${keyWord}&${searchFilter}${page}`);
+  const imgArray = await response.json();
+  return imgArray;
+}
+
+function notify(qty) {
+  const successMsg = `Hooray! We found ${qty} Images`;
+  Notiflix.Notify.success(successMsg);
+}
 
 function error() {
   const errorMsg = 'Sorry, there are no images matching your search query. Please try again.';
@@ -28,7 +46,7 @@ function error() {
   clearGallery();
 }
 
-function createImages(info) {
+function createGallery(info) {
   const markup = imageTpl(info);
   gallery.innerHTML = markup;
 }
@@ -36,7 +54,7 @@ function createImages(info) {
 function clearGallery() {
   gallery.innerHTML = '';
 }
-async function fetchImages(keyWord) {
-  const searchFilter = 'image_type=photo&orientation=horizontal&safesearch=true';
-  return await fetch(`${BASE_URL}?key=${KEY}&q=${keyWord}&${searchFilter}`);
-}
+
+openModal = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
--- src/js/modal.js
+++
@@ -1,8 +0,0 @@
-import simpleLightbox from 'simplelightbox';
-import '../../node_modules/simplelightbox/dist/simple-lightbox.min.css';
-
-export let openModal;
-
-openModal = new SimpleLightbox('.gallery a', {
-  captionDelay: 250,
-});
