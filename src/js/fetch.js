29ec2427a6190821eb50ee653bc597433a8a70fa
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-01-25 21:32:43 +0200

    added lightbox, fixed functional, added pagination

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,10 +1,14 @@
+import { imageLimitPerLoad } from './index.js';
+
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '25393494-37d8dd7e72c61fe26f3c6ef73';
 let page = 1;
 
 export async function fetchImages(keyWord) {
-  const searchFilter = 'image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=';
+  const searchFilter = `image_type=photo&orientation=horizontal&safesearch=true&per_page=${imageLimitPerLoad}&page=`;
   const response = await fetch(`${BASE_URL}?key=${KEY}&q=${keyWord}&${searchFilter}${page}`);
   const imgArray = await response.json();
+  page += 1;
+
   return imgArray;
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,56 @@
 import '../css/styles.css';
 import Notiflix from 'notiflix';
 import imageTpl from '../templates/imageTpl.hbs';
-import simpleLightbox from 'simplelightbox';
-import '../../node_modules/simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { fetchImages } from './fetch.js';
 
-const BASE_URL = 'https://pixabay.com/api/';
-const KEY = '25393494-37d8dd7e72c61fe26f3c6ef73';
 const gallery = document.querySelector('.gallery');
 const searchBox = document.querySelector('#search-form');
+const loadMoreBtn = document.querySelector('#load-more-btn');
 let imgToFind = '';
-let openModal = null;
+export const imageLimitPerLoad = 40;
+let totalImagesPerReq = 0;
 
-gallery.addEventListener('click', openModal);
 searchBox.addEventListener('submit', e => {
   e.preventDefault();
 
   imgToFind = searchBox.elements.searchQuery.value;
+
   fetchImages(imgToFind)
     .then(imgArr => {
-      if (imgArr.totalHits === 0) {
+      totalImagesPerReq = imgArr.totalHits;
+      if (totalImagesPerReq === 0) {
         throw new Error();
+      } else if (totalImagesPerReq <= imageLimitPerLoad) {
+        notify(totalImagesPerReq);
+        createGallery(imgArr.hits);
+      } else {
+        notify(totalImagesPerReq);
+        createGallery(imgArr.hits);
+        loadMoreBtn.classList.remove('is-hidden');
       }
-      notify(imgArr.totalHits);
-      createGallery(imgArr.hits);
     })
     .catch(error);
 });
 
-async function fetchImages(keyWord) {
-  const searchFilter = 'image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page';
-  let page = 1;
-  const response = await fetch(`${BASE_URL}?key=${KEY}&q=${keyWord}&${searchFilter}${page}`);
-  const imgArray = await response.json();
-  return imgArray;
-}
+gallery.addEventListener('click', e => {
+  e.preventDefault();
+
+  const lightbox = new SimpleLightbox('.gallery a');
+
+  lightbox.on('show.simplelightbox', e => {
+    lightbox.refresh();
+  });
+});
+
+loadMoreBtn.addEventListener('click', () => {
+  fetchImages(imgToFind)
+    .then(imgArr => {
+      createGallery(imgArr.hits);
+    })
+    .catch(error);
+});
 
 function notify(qty) {
   const successMsg = `Hooray! We found ${qty} Images`;
@@ -48,13 +65,9 @@ function error() {
 
 function createGallery(info) {
   const markup = imageTpl(info);
-  gallery.innerHTML = markup;
+  gallery.insertAdjacentHTML('beforeend', markup);
 }
 
 function clearGallery() {
   gallery.innerHTML = '';
 }
-
-openModal = new SimpleLightbox('.gallery a', {
-  captionDelay: 250,
-});
